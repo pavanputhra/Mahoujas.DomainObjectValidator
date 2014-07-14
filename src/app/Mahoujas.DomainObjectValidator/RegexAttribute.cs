5e2985dc366b49c85b480f897b77cc19bf1010d7
Pavan Kumar <pavan.kumar@mahoujas.com>
2014-07-14 16:59:46 +0530

    Added Regex validator.

---
+++ src/app/Mahoujas.DomainObjectValidator/RegexAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Mahoujas.DomainObjectValidator
+{
+    public class RegexAttribute : SinglePropertyOrCollectionValidationAttribute
+    {
+        public RegexAttribute(string pattern)
+        {
+            ConstraintTypes = new List<Type>
+            {
+                typeof(string),
+                typeof(ICollection<string>)
+            };
+
+            Pattern = pattern;
+        }
+
+        public override ValidationError Validate(object value)
+        {
+            var val = value as string;
+
+            if (val != null && !Regex.IsMatch(val, Pattern))
+            {
+                ErrorMessage = ErrorMessage ??
+                               string.Format("Property {0} must match specified regular expression.", PropertyName);
+                return new ValidationError(this);
+            }
+
+            return null;
+        }
+
+        public string Pattern { get; set; }
+    }
+}
---
+++ src/test/Mahoujas.DomainObjectValidator.Test/RegexTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mahoujas.DomainObjectValidator.Test
+{
+    [TestClass]
+    public class RegexTest
+    {
+        [TestMethod]
+        public void Regex_not_maching_must_fail()
+        {
+            var person = new Person
+            {
+                Email = "Hello_World"
+            };
+
+            var errors = person.ValidateDomainObject();
+            Assert.AreEqual(1,errors.Count);
+            Assert.AreEqual(true,errors.Any(e => e.ValidatorType == typeof(RegexAttribute)));
+        }
+
+        [TestMethod]
+        public void Regex_matches_must_pass()
+        {
+            var person = new Person
+            {
+                Email = "Hello_World@example.com"
+            };
+
+            var errors = person.ValidateDomainObject();
+            Assert.AreEqual(0, errors.Count);
+        }
+
+        private class Person
+        {
+            [Regex(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]
+            public string Email { get; set; }
+        }
+    }
+}

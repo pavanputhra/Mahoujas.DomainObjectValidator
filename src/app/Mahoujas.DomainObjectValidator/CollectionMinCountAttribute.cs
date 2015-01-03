bcaf32c5a84f6771d21d25f1f33016a747a083d7
Pavan Kumar <pavan.kumar@mahoujas.com>
2015-01-03 12:12:38 +0530

    Mincount for collection validator.

---
+++ src/app/Mahoujas.DomainObjectValidator/CollectionMinCountAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahoujas.DomainObjectValidator
+{
+    public class CollectionMinCountAttribute : SinglePropertyValidationAttribute
+    {
+        public CollectionMinCountAttribute(int length)
+        {
+            ConstraintTypes = new List<Type>
+            {
+                typeof (ICollection)
+            };
+            Length = length;
+        }
+
+        public override ValidationError Validate(object value)
+        {
+            var val = value as ICollection;
+
+            if (val != null && val.Count < Length)
+            {
+                ErrorMessage = ErrorMessage ?? string.Format("Collection {0} must contain {1} or more items.", PropertyName, Length);
+                return new ValidationError(this);
+            }
+
+            return null;
+        }
+
+        public int Length { get; set; }
+    }
+}
---
+++ src/test/Mahoujas.DomainObjectValidator.Test/CollectionMinCountTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mahoujas.DomainObjectValidator.Test
+{
+    [TestClass]
+    public class CollectionMinCountTest
+    {
+        [TestMethod]
+        public void Array_Count_Less_Than_Specified_Must_Fail()
+        {
+            var person = new PersonArray
+            {
+                Friends = new string[] {"Ramesh", "Suresh"}
+            };
+
+            var errors = person.ValidateDomainObject();
+            Assert.AreEqual(1,errors.Count);
+            Assert.AreEqual(true,errors.Any(e => e.ValidatorType == typeof(CollectionMinCountAttribute)));
+        }
+
+        [TestMethod]
+        public void Array_Count_Equal_To_Specified_Must_Pass()
+        {
+            var person = new PersonArray
+            {
+                Friends = new string[] { "Ramesh", "Suresh", "Giresh" }
+            };
+
+            var errors = person.ValidateDomainObject();
+            Assert.AreEqual(0, errors.Count);
+        }
+
+        private class PersonArray
+        {
+            [CollectionMinCount(3)]
+            public string [] Friends { get; set; }
+        }
+    }
+}
